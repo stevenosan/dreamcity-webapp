42f2458ff015125b5d8747a43b4930238ef4d662
stev <stevenosan@gmail.com>
2021-12-15 17:23:29 -0500

    starting to move the webflow stuff into react components

---
+++ candy-machine/src/pages/intro/Intro.tsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+    return (
+        <div id="intro-section" className="intro wf-section">
+        <div className="columns w-row">
+          <div className="intro-column w-col w-col-6">
+            <div className="intro-copy">
+              <h1 className="heading">Welcome to Dream City</h1>
+              <p className="paragraph">Dream city is a gigacity filled with underground gangs, corporate spies, and everything in-between. If you can hack it here, your wildest dreams are possible.</p>
+            </div>
+          </div>
+          <div className="column-2 w-col w-col-6"><img src="images/Site_BG.jpg" loading="lazy" sizes="(max-width: 767px) 100vw, 50vw" height="634" srcSet="images/Site_BG-p-500.jpeg 500w, images/Site_BG-p-800.jpeg 800w, images/Site_BG-p-1080.jpeg 1080w, images/Site_BG-p-1600.jpeg 1600w, images/Site_BG-p-2000.jpeg 2000w, images/Site_BG-p-2600.jpeg 2600w, images/Site_BG-p-3200.jpeg 3200w, images/Site_BG.jpg 4096w" alt="" className="image-2" /></div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ candy-machine/src/pages/navbar/NavBar.tsx
@@ -0,0 +1,27 @@
+
+
+function App() {
+  return (
+    <div className="navigation wf-section">
+    <a href="#intro-section" className="w-inline-block"><img src="images/DreamCityLogo.png" loading="lazy" alt="" className="image" /></a>
+    <ul role="list" className="list w-list-unstyled">
+      <li className="nav-item">
+        <a href="#orphans-section" className="list-link">The Orphans</a>
+      </li>
+      <li className="nav-item">
+        <a href="#roadmap-section" className="list-link">Roadmap</a>
+      </li>
+      <li className="nav-item">
+        <a href="#ourteam-section" className="list-link">Our Team</a>
+      </li>
+      <li className="nav-item">
+        <a href="#faqs-section" className="list-link">FAQs</a>
+      </li>
+    </ul>
+    <a href="https://twitter.com/dreamcitynft" target="_blank" className="link-block w-inline-block"><img src="images/twitter.svg" loading="lazy" alt="" className="image-6" /></a>
+    <a href="https://discord.gg/Hkg3NXEX6b" className="link-block-2 w-inline-block"><img src="images/discord.svg" loading="lazy" alt="" className="image-7" /></a>
+  </div>
+  );
+}
+
+export default App;

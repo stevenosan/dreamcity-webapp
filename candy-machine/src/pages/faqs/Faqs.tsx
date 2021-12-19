60e15b8e8a30c1001e7f6c2a7072704dcbfaab04
Carl DeCaire <cdecaire@gmail.com>
2021-12-19 12:09:06 -0500

    convert html to tsx
    I definitely know what i'm doing.

--- candy-machine/src/pages/faqs/Faqs.tsx
+++ candy-machine/src/pages/faqs/Faqs.tsx
@@ -2,78 +2,78 @@
 
 function Faqs() {
     return (
-        <div id="faqs-section" className="section-3 wf-section">
-        <div className="container-3 w-container">
-          <h2 className="team-heading">// FAQs</h2>
-          <div className="columns-2 w-row">
-            <div className="column-7 w-col w-col-4">
-              <div className="text-block-3">What is Dream City?</div>
-              <p className="paragraph-3">Dream City is an NFT brand powered by the Solana blockchain.</p>
+          <section id="faqs-section" className="faq">
+            <div className="rows">
+              <h2 className="team-heading">// FAQs</h2>
             </div>
-            <div className="column-8 w-col w-col-4">
-              <div className="text-block-3">When is the sale/drop date?</div>
-              <p className="paragraph-3">Currently we’re hard at work getting everything running. Follow us on Twitter or join our Discord to be up-to-date on all information.</p>
-            </div>
-            <div className="w-col w-col-4">
-              <div className="text-block-3">How many traits in Dream City?</div>
-              <p className="paragraph-3">The first edition of The Orphans will contain over 100 traits such as masks, tattoos, eyes, clothing, and more.</p>
-            </div>
-          </div>
-          <div className="columns-2 w-row">
-            <div className="column-9 w-col w-col-4">
-              <div className="text-block-3">When will the full website launch?</div>
-              <p className="paragraph-3">Official launch will be posted on Twitter and Discord as we continue to build out Dream City.</p>
-            </div>
-            <div className="column-10 w-col w-col-4">
-              <div className="text-block-3">How can I reach the team?</div>
-              <p className="paragraph-3">Most suggestions can be dropped in Discord under “Ideas and Feedback”. For other inquiry DM on Twitter.</p>
-            </div>
-            <div className="w-col w-col-4">
-              <div className="text-block-3">How to buy a Dream City NFT?</div>
-              <p className="paragraph-3">Official launch details will be provided through this website, Twitter, and Discord.</p>
-            </div>
-          </div>
-          <div className="columns-2 w-row">
-            <div className="column-11 w-col w-col-4">
-              <div className="text-block-3">Will there be an aftermarket?</div>
-              <p className="paragraph-3">TBD</p>
-            </div>
-            <div className="column-12 w-col w-col-4">
-              <div className="text-block-3">How much is an Orphan?</div>
-              <p className="paragraph-3">TBD</p>
-            </div>
-            <div className="w-col w-col-4">
-              <div className="text-block-3">Where can I see my purchases?</div>
-              <p className="paragraph-3">You can view them directly within Phantom wallet’s Collectible tab.</p>
-            </div>
-          </div>
-          <div className="columns-2 w-row">
-            <div className="column-13 w-col w-col-4">
-              <div className="text-block-3">How do I create a Phantom Wallet?</div>
-              <p className="paragraph-3">Go to phantom.app, download the Phantom Wallet extension. Once installed run the program to create a new wallet.</p>
-            </div>
-            <div className="column-14 w-col w-col-4">
-              <div className="text-block-3">What blockchain does Dream City utilize?</div>
-              <p className="paragraph-3">Dream City will be exclusive to the Solana Blockchain.</p>
-            </div>
-            <div className="w-col w-col-4">
-              <div className="text-block-3">Where is the digital artwork stored?</div>
-              <p className="paragraph-3">The digital art will be stored on Arweave, a decentralized data storage that backs data with sustainable and perpetual endowments, allowing users and developers to truly store data forever.</p>
-            </div>
-          </div>
-          <div className="columns-2 w-row">
-            <div className="column-15 w-col w-col-4">
-              <div className="text-block-3">Will there be resale royalties?</div>
-              <p className="paragraph-3">Yes, the right percentage is being determined.</p>
-            </div>
-            <div className="column-16 w-col w-col-4">
-              <div className="text-block-3">Will you be giving to charity?</div>
-              <p className="paragraph-3">Yes, we’re currently going through the selection process with the team. Details will come soon.</p>
-            </div>
-            <div className="column-17 w-col w-col-4"></div>
-          </div>
-        </div>
-      </div>
+              <div className="rows">
+                <div className="faq-item">
+                  <h4>What is Dream City?</h4>
+                  <p>Dream City is an NFT brand powered by the Solana blockchain.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>When is the sale/drop date?</h4>
+                  <p>Currently we’re hard at work getting everything running. Follow us on Twitter or join our Discord to be up-to-date on all information.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>How many traits in Dream City?</h4>
+                  <p>The first edition of The Orphans will contain over 100 traits such as masks, tattoos, eyes, clothing, and more.</p>
+                </div>
+              </div>
+              <div className="rows">
+                <div className="faq-item">
+                  <h4>When will the full website launch?</h4>
+                  <p>Official launch will be posted on Twitter and Discord as we continue to build out Dream City.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>How can I reach the team?</h4>
+                  <p>Most suggestions can be dropped in Discord under “Ideas and Feedback”. For other inquiry DM on Twitter.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>How to buy a Dream City NFT?</h4>
+                  <p>Official launch details will be provided through this website, Twitter, and Discord.</p>
+                </div>
+              </div>
+              <div className="rows">
+                <div className="faq-item">
+                  <h4>Will there be an aftermarket?</h4>
+                  <p>TBD</p>
+                </div>
+                <div className="faq-item">
+                  <h4>How much is an Orphan?</h4>
+                  <p>TBD</p>
+                </div>
+                <div className="faq-item">
+                  <h4>Where can I see my purchases?</h4>
+                  <p>You can view them directly within Phantom wallet’s Collectible tab.</p>
+                </div>
+              </div>
+              <div className="rows">
+                <div className="faq-item">
+                  <h4>How do I create a Phantom Wallet?</h4>
+                  <p>Go to phantom.app, download the Phantom Wallet extension. Once installed run the program to create a new wallet.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>What blockchain does Dream City utilize?</h4>
+                  <p>Dream City will be exclusive to the Solana Blockchain.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>Where is the digital artwork stored?</h4>
+                  <p>The digital art will be stored on Arweave, a decentralized data storage that backs data with sustainable and perpetual endowments, allowing users and developers to truly store data forever.</p>
+                </div>
+              </div>
+              <div className="rows">
+                <div className="faq-item">
+                  <h4>Will there be resale royalties?</h4>
+                  <p>Yes, the right percentage is being determined.</p>
+                </div>
+                <div className="faq-item">
+                  <h4>Will you be giving to charity?</h4>
+                  <p>Yes, we’re currently going through the selection process with the team. Details will come soon.</p>
+                </div>
+                <div className="faq-item">&nbsp;</div>
+              </div>
+          </section>
 
     );
   }
--- candy-machine/src/pages/navbar/NavBar.tsx
+++ candy-machine/src/pages/navbar/NavBar.tsx
@@ -2,25 +2,29 @@
 
 function NavBar() {
   return (
-    <div className="navigation wf-section">
-    <a href="#intro-section" className="w-inline-block"><img src="images/DreamCityLogo.png" loading="lazy" alt="" className="image" /></a>
-    <ul role="list" className="list w-list-unstyled">
-      <li className="nav-item">
-        <a href="#orphans-section" className="list-link">The Orphans</a>
-      </li>
-      <li className="nav-item">
-        <a href="#roadmap-section" className="list-link">Roadmap</a>
-      </li>
-      <li className="nav-item">
-        <a href="#ourteam-section" className="list-link">Our Team</a>
-      </li>
-      <li className="nav-item">
-        <a href="#faqs-section" className="list-link">FAQs</a>
-      </li>
-    </ul>
-    <a href="https://twitter.com/dreamcitynft" target="_blank" className="link-block w-inline-block"><img src="images/twitter.svg" loading="lazy" alt="" className="image-6" /></a>
-    <a href="https://discord.gg/Hkg3NXEX6b" className="link-block-2 w-inline-block"><img src="images/discord.svg" loading="lazy" alt="" className="image-7" /></a>
-  </div>
+    <header>
+      <nav className="navigation">
+        <a href="#intro-section" className="logo">Dream City</a>
+        <ul className="main-nav">
+          <li className="nav-item">
+            <a href="#orphans-section" className="list-link">The Orphans</a>
+          </li>
+          <li className="nav-item">
+            <a href="#roadmap-section" className="list-link">Roadmap</a>
+          </li>
+          <li className="nav-item">
+            <a href="#ourteam-section" className="list-link">Our Team</a>
+          </li>
+          <li className="nav-item">
+            <a href="#faqs-section" className="list-link">FAQs</a>
+          </li>
+        </ul>
+        <ul className="social">
+          <li><a href="https://twitter.com/dreamcitynft" target="_blank" className="social-link twitter">Twitter</a></li>
+          <li><a href="https://discord.gg/Hkg3NXEX6b" className="social-link discord">Discord</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 

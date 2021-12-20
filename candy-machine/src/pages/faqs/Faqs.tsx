532b8f8ec60e0eb655a8626bfbd38996a32223c6
Carl DeCaire <cdecaire@gmail.com>
2021-12-20 14:28:27 -0500

    Responsive Styles

--- candy-machine/src/pages/faqs/Faqs.tsx
+++ candy-machine/src/pages/faqs/Faqs.tsx
@@ -19,8 +19,6 @@ function Faqs() {
                   <h4>How many traits in Dream City?</h4>
                   <p>The first edition of The Orphans will contain over 100 traits such as masks, tattoos, eyes, clothing, and more.</p>
                 </div>
-              </div>
-              <div className="rows">
                 <div className="faq-item">
                   <h4>When will the full website launch?</h4>
                   <p>Official launch will be posted on Twitter and Discord as we continue to build out Dream City.</p>
@@ -33,8 +31,6 @@ function Faqs() {
                   <h4>How to buy a Dream City NFT?</h4>
                   <p>Official launch details will be provided through this website, Twitter, and Discord.</p>
                 </div>
-              </div>
-              <div className="rows">
                 <div className="faq-item">
                   <h4>Will there be an aftermarket?</h4>
                   <p>TBD</p>
@@ -47,8 +43,6 @@ function Faqs() {
                   <h4>Where can I see my purchases?</h4>
                   <p>You can view them directly within Phantom wallet’s Collectible tab.</p>
                 </div>
-              </div>
-              <div className="rows">
                 <div className="faq-item">
                   <h4>How do I create a Phantom Wallet?</h4>
                   <p>Go to phantom.app, download the Phantom Wallet extension. Once installed run the program to create a new wallet.</p>
@@ -61,8 +55,6 @@ function Faqs() {
                   <h4>Where is the digital artwork stored?</h4>
                   <p>The digital art will be stored on Arweave, a decentralized data storage that backs data with sustainable and perpetual endowments, allowing users and developers to truly store data forever.</p>
                 </div>
-              </div>
-              <div className="rows">
                 <div className="faq-item">
                   <h4>Will there be resale royalties?</h4>
                   <p>Yes, the right percentage is being determined.</p>
@@ -71,7 +63,6 @@ function Faqs() {
                   <h4>Will you be giving to charity?</h4>
                   <p>Yes, we’re currently going through the selection process with the team. Details will come soon.</p>
                 </div>
-                <div className="faq-item">&nbsp;</div>
               </div>
           </section>
 

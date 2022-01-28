e39caea96740e57749f93908bc8533b5897db5b1
stev <stevenosan@gmail.com>
2022-01-28 08:21:09 -0500

    maybe this fixed it? gotta include that .env

--- candy-machine/webpack.config.js
+++ candy-machine/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -44,6 +45,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "public", "index.html"),
         }),
+        new Dotenv()
     ],
     node: {
         fs: 'empty'

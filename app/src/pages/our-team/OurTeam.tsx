172a2971584526bde9a67b532cb29ca50c97fb4c
Carl DeCaire <cdecaire@gmail.com>
2022-02-15 09:14:18 -0500

    DC-Redesign-Update

--- app/src/pages/our-team/OurTeam.tsx
+++ app/src/pages/our-team/OurTeam.tsx
@@ -3,63 +3,97 @@
 function OurTeam() {
     return (
       <section id="ourteam-section" className="team">
-          <div className="rows">
-            <h2 className="team-heading">// Our Team</h2>
+      <h2 className="team-heading">Our Team</h2>
+        <div className="rows">
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/brandon-dc-art.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Brandon Mullins</p>
+                <p className="team-position">Founder / Art / Design / Dev</p>
+              </div>
+              <figcaption>
+                <p className="team-name">Brandon Mullins</p>
+                <p className="team-position">Founder / Art / Design / Dev</p>
+                Brandon has been in the creative services field around 20 years now. Graphic design, advertising, web, app, software, and now what’s known more formally as product or UX design. From the art side of things he’s been drawing before he could write and is also very active in the Solana 1/1 art scene.
+                <a href="https://twitter.com/wakeupbrandon" target="_blank">@wakeupbrandon</a>
+              </figcaption>
+            </figure>
           </div>
-            <div className="rows">
-              <div className="team-member">
-                <a href="https://twitter.com/wakeupbrandon" target="_blank">
-                  <img src="images/brandon-dc-art.png" alt="" />
-                  <div className="team-dox">
-                    <p className="team-name">Brandon Mullins</p>
-                    <p className="team-position">Founder / Art / Design / Dev</p>
-                  </div>
-                </a>
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/michelle-dc-lore.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Michelle Amos</p>
+                <p className="team-position">Lore Master / Project Manager</p>
               </div>
-              <div className="team-member">
-                <img src="images/michelle-dc-lore.png" alt="" />
-                <div className="team-dox">
-                  <p className="team-name">Michelle Amos</p>
-                  <p className="team-position">Lore Master</p>
-                </div>
+              <figcaption>
+                <p className="team-name">Michelle Amos</p>
+                <p className="team-position">Lore Master / Project Manager</p>
+                Michelle is an artist and writer who loves all things sci-fi and fantasy related. She’s excited to put her nerd credentials to work as DC Loremaster, while still being able to use skills from her day job as a Systems Analyst/Project Manager to act as PM for Dream City.
+              </figcaption>
+            </figure>
+          </div>
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/matt-dc-marketing.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Matt Cook</p>
+                <p className="team-position">Community Engagement</p>
               </div>
-              <div className="team-member">
-                <a href="https://twitter.com/dreamcitynft" target="_blank">
-                  <img src="images/matt-dc-marketing.png" alt="" />
-                  <div className="team-dox">
-                    <p className="team-name">Matt Cook</p>
-                    <p className="team-position">Community Engagement</p>
-                  </div>
-                </a>
+              <figcaption>
+                <p className="team-name">Matt Cook</p>
+                <p className="team-position">Community Engagement</p>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/carl-dc-partnerships.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Carl DeCaire</p>
+                <p className="team-position">Partnerships / Advisor</p>
               </div>
-              <div className="team-member">
-                <a href="https://twitter.com/empirikal" target="_blank">
-                  <img src="images/carl-dc-partnerships.png" alt="" />
-                  <div className="team-dox">
-                    <p className="team-name">Carl DeCaire</p>
-                    <p className="team-position">Partnerships / Advisor</p>
-                  </div>
-                </a>
+              <figcaption>
+                <p className="team-name">Carl DeCaire</p>
+                <p className="team-position">Partnerships / Advisor</p>
+                Carl has over 20 years designing and building traditional web2 products and services, recently transitioning to web3 as a Product Design Lead at Metaplex.com helping to create the NFT standard for Solana.
+                <a href="https://twitter.com/empirikal" target="_blank">@empirikal</a>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/steve-dc-engineer.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Steve Nosan</p>
+                <p className="team-position">Engineer</p>
               </div>
-              <div className="team-member">
-                <img src="images/steve-dc-engineer.png" alt="" />
-                <div className="team-dox">
-                  <p className="team-name">Steve Nosan</p>
-                  <p className="team-position">Engineer</p>
-                </div>
+              <figcaption>
+                <p className="team-name">Steve Nosan</p>
+                <p className="team-position">Engineer</p>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="team-member">
+            <figure className="bio">
+              <img src="images/joe-dc-moderator.png" alt="" />
+              <div className="team-dox">
+                <p className="team-name">Joe Santavicca</p>
+                <p className="team-position">Community Moderator</p>
               </div>
-              <div className="team-member">
-                <img src="images/joe-dc-moderator.png" alt="" />
-                <div className="team-dox">
-                  <p className="team-name">Joe Santavicca</p>
-                  <p className="team-position">Community Moderator</p>
-                </div>
-              </div>
-            </div>
-            <div className="rows">
-              <p>Our team is a small group of friends that have known each other in real life for over a decade (in some cases longer). We love creativity and are excited about the NFT space for exploring that creativity.</p>
-            </div>
-        </section>
+              <figcaption>
+                <p className="team-name">Joe Santavicca</p>
+                <p className="team-position">Community Moderator</p>
+                Joe used to spend all day in restaurants as an Operations Director, but left to go back to school and play in the woods. His #1 hobby is talking to people, which makes him the perfect discord mod.
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div className="rows">
+          <p>Our team is a small group of friends that have known each other in real life for over a decade (in some cases longer). We love creativity and are excited about the NFT space for exploring that creativity.</p>
+        </div>
+      </section>
     );
   }
   
---
+++ app/src/pages/process/Process.tsx
@@ -0,0 +1,50 @@
+
+
+function Process() {
+    return (
+      <section id="process-section" className="process">
+        <div className="columns">
+          <div className="left-column">
+            <div className="copy">
+              <div>
+                <h2>The Process</h2>
+                <ul>
+                  <li>
+                    <h3>Part 1: The prompt</h3>
+                    We set the ground work for you. Nobody really likes to work from a blank canvas. We provide a prompt and some details. You fill in the void.
+                  </li>
+                  <li>
+                    <h3>Part 2: The vote</h3>
+                    Holders of the NFT for Dream City will vote on their favorite stories. The community will help drive and shape the world of Dream City.
+                  </li>
+                  <li>
+                    <h3>Part 3: The craft</h3>
+                    Writers and Artists will pair up and work on the winning story. It’ll be included in the anthology comic with other stories.
+                  </li>
+                  <li>
+                    <h3>FUTURE: The gift</h3>
+                    Ideally, we’d like to be able to reward people for their efforts. We have a few ideas, Royalties, Tokens, and Air Drops to name a few. More details TBA.
+                  </li>
+                </ul>
+                <h2>What Else?</h2>
+                <p className="subhead">all of that sounds like future work</p>
+                <p>It mostly is. For right now, we have a free prologue comic and another couple of holder-gated short stories in the works. We also have A LOT of lore written and ready to publish which will also be free to view.</p>
+                <h2>other things you should know</h2>
+                <p className="subhead">we work slow on purpose</p>
+                <p>Everyone that’s currently on the team has a full-time day job. This is a passion project for us. We want to be nimble, but we also want time to consider options and how it impacts or helps fulfill our vision. </p>
+                <p className="subhead">we value inclusivity</p>
+                <p>We want everyone to feel welcome in our community. Additionally everyone should feel as though whatever Dream City character they’re embodying it would be welcome in our ficticious world.</p>
+                <a href="#roadmap-section" className="button-primary">View Our Roadmap</a>
+              </div>
+            </div>
+          </div>
+          <div className="right-column image">
+            <img src="images/dc_process.png" className="image" />
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Process;
+  
